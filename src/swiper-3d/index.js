b6df53cf6a8ffbe95c83cec3ab9bc33454b8a33b
china_wangyu <china_wangyu@aliyun.com>
2020-01-03 17:59:00 +0800

    更新swiper-3d效果

--- src/swiper-3d/index.js
+++ src/swiper-3d/index.js
@@ -6,7 +6,27 @@ Component({
   properties: {
     swiperH:{
       type:String,
-      value:"70vh"
+      value:"40vh"
+    },
+    swiperW: {
+      type: String,
+      value: "500rpx"
+    },
+    bannerData: {
+      type: Array,
+      value: [
+        {
+          'id': 1, 'focus': 'https://www.duoguyu.com/dist/flip/flipImg-1.jpg', 'img': 'https://www.duoguyu.com/dist/flip/flipImg-s1.jpg', 'title': '阿丽塔：战斗天使', 'isOpenFilp': false, 'lines': '“我们应该到那里去，我们属于那里。” \n“不，我们不属于任何地方，除了彼此身边。”', 'score': '7.6', 'releaseDate': '2019/02/22', 'otherInfo': 'Alita: Battle Angel'
+        },
+        {
+          'id': 2, 'focus': 'https://www.duoguyu.com/dist/flip/flipImg-2.jpg', 'img': 'https://www.duoguyu.com/dist/flip/flipImg-s2.jpg', 'title': '我不是药神', 'isOpenFilp': false, 'lines': '你是不是看不起我？ \n\n是…以前是…', 'score': '9.0', 'releaseDate': '2018/07/05', 'otherInfo': 'Dying to Survive'
+        },
+        { 'id': 3, 'focus': 'https://www.duoguyu.com/dist/flip/flipImg-3.jpg', 'img': 'https://www.duoguyu.com/dist/flip/flipImg-s3.jpg', 'title': '风语咒', 'isOpenFilp': false, 'lines': '人法地法天法道法自然，传说中的神功根本就不是练出来的，人与自然本就是一体的。\n\n我即是自然，自然即是我。\n我在驭风，风在驭我。', 'score': '6.9', 'releaseDate': '2018/08/03', 'otherInfo': 'The Wind Guardians' },
+        { 'id': 4, 'focus': 'https://www.duoguyu.com/dist/flip/flipImg-4.jpg', 'img': 'https://www.duoguyu.com/dist/flip/flipImg-s4.jpg', 'title': '飞驰人生', 'isOpenFilp': false, 'lines': '巴音布鲁克，1462道弯，109公里。耍小聪明，赢得了一百米，赢不了一百公里。\n\n你问我绝招？\n绝招只有两个字：奉献。\n就是把你的全部奉献给你所热爱的一切。\n你并不是征服了这片土地，你只是战胜了你的对手。', 'score': '7.2', 'releaseDate': '2019/02/05', 'otherInfo': 'Pegasus' },
+        {
+          'id': 5, 'focus': 'https://www.duoguyu.com/dist/flip/flipImg-5.jpg', 'img': 'https://www.duoguyu.com/dist/flip/flipImg-s5.jpg', 'title': '大黄蜂', 'isOpenFilp': false, 'lines': '"You kissed me!" \n"On the cheek."\n"Still counts, still counts."', 'score': '7.0', 'releaseDate': '2019/01/04', 'otherInfo': 'Bumblebee'
+        },
+      ],
     }
   },
   lifetimes: {
@@ -20,13 +40,6 @@ Component({
    * 组件的初始数据
    */
   data: {
-    imgUrls: [
-      "https://www.duoguyu.com/dist/flip/flipImg-1.jpg",
-      "https://www.duoguyu.com/dist/flip/flipImg-2.jpg",
-      "https://www.duoguyu.com/dist/flip/flipImg-3.jpg",
-      "https://www.duoguyu.com/dist/flip/flipImg-4.jpg",
-      "https://www.duoguyu.com/dist/flip/flipImg-5.jpg",
-    ],
     swiperIndex: 0,
     current: 0,
     animationData: {},
